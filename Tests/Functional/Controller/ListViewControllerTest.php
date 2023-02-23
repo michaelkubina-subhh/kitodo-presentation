e4b5e9144276f131081aa0fb03ca88c1fea9499b
haogatyp <gatermann@effective-webwork.de>
2023-02-23 16:24:40 +0100

    Extend output.

--- Tests/Functional/Controller/ListViewControllerTest.php
+++ Tests/Functional/Controller/ListViewControllerTest.php
@@ -3,41 +3,64 @@
 namespace Kitodo\Dlf\Tests\Functional\Controller;
 
 use Kitodo\Dlf\Common\MyTest;
+use Kitodo\Dlf\Common\Solr;
 use Kitodo\Dlf\Controller\ListViewController;
+use Kitodo\Dlf\Domain\Model\SolrCore;
+use Kitodo\Dlf\Domain\Repository\CollectionRepository;
+use Kitodo\Dlf\Domain\Repository\DocumentRepository;
+use Kitodo\Dlf\Domain\Repository\MetadataRepository;
+use Kitodo\Dlf\Domain\Repository\SolrCoreRepository;
 use Prophecy\PhpUnit\ProphecyTrait;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Response;
 use TYPO3\CMS\Extbase\Mvc\View\GenericViewResolver;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Mvc\Request;
-use Kitodo\Dlf\Domain\Repository\MailRepository;
 use Kitodo\Dlf\Tests\Functional\FunctionalTestCase;
+use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 
 class ListViewControllerTest extends FunctionalTestCase
 {
     /**
-     * @var MailRepository
+     * @var CollectionRepository
      */
-    protected MailRepository $mailRepository;
+    protected $collectionRepository;
 
     /**
-     * @var Prophet;
+     * @var DocumentRepository
      */
-    protected Prophet $prophet;
+    protected $documentRepository;
+
+    /**
+     * @var SolrCoreRepository
+     */
+    protected $solrCoreRepository;
+
+    /**
+     * @var MetadataRepository
+     */
+    protected $metadataRepository;
+
+    /**
+     * @var persistenceManager
+     */
+    protected $persistenceManager;
 
     public function setUp(): void
     {
         parent::setUp();
 
-      //  $this->importDataSet(__DIR__ . '/../../Fixtures/Repository/mail.xml');
+        $this->persistenceManager = $this->objectManager->get(PersistenceManager::class);
 
-   /*     $collectionRepository
-             CollectionRepository $collectionRepository
-            $metadataRepository
-                MetadataRepository $metadataRepository
-    */
+        $this->collectionRepository = $this->initializeRepository(CollectionRepository::class, 20000);
+        $this->documentRepository = $this->initializeRepository(DocumentRepository::class, 20000);
+        $this->solrCoreRepository = $this->initializeRepository(SolrCoreRepository::class, 20000);
+        $this->metadataRepository = $this->initializeRepository(MetadataRepository::class, 20000);
+
+        $this->importDataSet(__DIR__ . '/../../Fixtures/Controller/documents_1.xml');
+        $this->importDataSet(__DIR__ . '/../../Fixtures/Controller/metadata.xml');
     }
 
     /**
@@ -46,15 +69,30 @@ class ListViewControllerTest extends FunctionalTestCase
      */
     public function canMainAction(): void
     {
+        $core = $this->createSolrCore();
+        $this->importSolrDocuments($core->solr, __DIR__ . '/../../Fixtures/Common/documents_1.solr.json');
+
+        // TODO: Must be available inside the main action $this->settings
+        $settings = [
+            'solrcore' => $core->solr->core,
+            'storagePid' => 20000,
+        ];
+
         $request = new Request();
         $request->setPluginName('ListView');
         $request->setControllerExtensionName('ListViewControllerTest');
         $request->setControllerName('ListView');
         $request->setFormat('html');
+        $request->setControllerExtensionName('dlf');
 
         $request->setControllerActionName('main');
-        $request->setArgument('searchParameter', 'test');
-        $request->setArgument('searchParametefgfr', 'test');
+        /* TODO: $request->setArgument('tx_dlf', ['double' => 10, 'page' => 1234]); */
+        /* TODO: $request->setArgument('page', 1234); */
+        // $request->setArgument('searchParameter', ['query' => '*', 'collection' => '1101']);
+        $request->setArgument('searchParameter', ['query' => '*']);
+
+        $request->setArgument('widgetPage', ['currentPage' => 1]);
+        // Or is it like this: $request->setArgument('@widget_0', 1);
 
         /** @var ObjectManager $objectManager */
         $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
@@ -64,6 +102,7 @@ class ListViewControllerTest extends FunctionalTestCase
         $response =  $objectManager->get(Response::class);
 
         $GLOBALS['TSFE']->fe_user = new FrontendUserAuthentication();
+        $GLOBALS['TSFE']->id = 1234; // TODO: Value not available in controller action.
 
         $viewResolverMock = $this->getMockBuilder( GenericViewResolver::class)->disableOriginalConstructor()->getMock();
         $view = new StandaloneView();
@@ -75,6 +114,30 @@ class ListViewControllerTest extends FunctionalTestCase
                 page: {viewData.requestData.page}
                 double: {viewData.requestData.double}
                 </f:spaceless>
+
+                widgetPage: {widgetPage}
+
+                lastSearch.query: {lastSearch.query}
+
+                sortableMetadata:
+                <f:for each="{sortableMetadata}" as="sMetadata">
+                    {sMetadata.uid}/{sMetadata.label}
+                </f:for>
+
+                listedMetadata:
+                <f:for each="{listedMetadata}" as="lMetadata">
+                    {lMetadata.uid}/{lMetadata.label}
+                </f:for>
+
+                <f:for each="{documents}" as="paginatedDocuments">
+                  <f:for each="{paginatedDocuments}" as="document" iteration="docIterator">
+                    <f:variable name="docTitle" value="{f:if(condition:\'{document.title}\', then:\'{document.title}\', else:\'{document.metsOrderlabel}\')}" />
+                    document: {docTitle}
+                  </f:for>
+                </f:for>
+
+                numResults: {numResults}
+
             </html>'
         );
 
@@ -86,9 +149,23 @@ class ListViewControllerTest extends FunctionalTestCase
         $subject->processRequest($request, $response);
 
         $output =  $response->getContent();
-echo $output;
-        /*
-        https://dlfdemo.ddev.site/?tx_dlf_listview%5Baction%5D=main&tx_dlf_listview%5Bcontroller%5D=ListView&tx_dlf_listview%5BsearchParameter%5D%5Bfulltext%5D=0&tx_dlf_listview%5BsearchParameter%5D%5Border%5D=&tx_dlf_listview%5BsearchParameter%5D%5BorderBy%5D=&tx_dlf_listview%5BsearchParameter%5D%5Bquery%5D=%2A&tx_dlf_listview%5BwidgetPage%5D%5BcurrentPage%5D=1&cHash=37535ceb0e731d1d9ae297eff2451a5c
-          */
+
+        echo $output;
+
+        // TODO: assertions
+    }
+
+    protected function createSolrCore(): object
+    {
+        $coreName = Solr::createCore();
+        $solr = Solr::getInstance($coreName);
+
+        $model = GeneralUtility::makeInstance(SolrCore::class);
+        $model->setLabel('Testing Solr Core');
+        $model->setIndexName($coreName);
+        $this->solrCoreRepository->add($model);
+        $this->persistenceManager->persistAll();
+
+        return (object) compact('solr', 'model');
     }
 }
