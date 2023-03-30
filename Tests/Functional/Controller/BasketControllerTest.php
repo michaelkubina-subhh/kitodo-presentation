31eb51bca04f4629c53d93f623cd69ce770e438d
Oliver Stöhr <stoehr@effective-webwork.de>
2023-03-30 21:32:07 +0200

    Add tests for BasketController

--- Tests/Functional/Controller/BasketControllerTest.php
+++ Tests/Functional/Controller/BasketControllerTest.php
@@ -12,13 +12,27 @@
 
 namespace Kitodo\Dlf\Tests\Functional\Controller;
 
-use Kitodo\Dlf\Tests\Functional\FunctionalTestCase;
+use Kitodo\Dlf\Controller\BasketController;
+use TYPO3\CMS\Extbase\Mvc\Exception\StopActionException;
+use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 
-class BasketControllerTest extends FunctionalTestCase
+class BasketControllerTest extends AbstractControllerTest
 {
+
+    static array $databaseFixtures = [
+        __DIR__ . '/../../Fixtures/Controller/pages.xml',
+        __DIR__ . '/../../Fixtures/Controller/solrcores.xml'
+    ];
+
+    static array $solrFixtures = [
+        __DIR__ . '/../../Fixtures/Controller/documents.solr.json'
+    ];
+
     public function setUp(): void
     {
         parent::setUp();
+        $this->setUpData(self::$databaseFixtures);
+        $this->setUpSolr(4, 2, self::$solrFixtures);
     }
 
     /**
@@ -26,7 +40,14 @@ class BasketControllerTest extends FunctionalTestCase
      */
     public function canAddAction()
     {
-        // TODO implement
+        $controller = $this->setUpController(BasketController::class, []);
+        $request = $this->setUpRequest('add');
+        $response = $this->getResponse();
+        $GLOBALS['TSFE']->fe_user = new FrontendUserAuthentication();
+        $GLOBALS['TSFE']->fe_user->id = 1;
+
+        $this->expectException(StopActionException::class);
+        $controller->processRequest($request, $response);
     }
 
     /**
@@ -34,7 +55,13 @@ class BasketControllerTest extends FunctionalTestCase
      */
     public function canBasketAction()
     {
-        // TODO implement
+        $controller = $this->setUpController(BasketController::class, []);
+        $request = $this->setUpRequest('add');
+        $response = $this->getResponse();
+        $GLOBALS['TSFE']->fe_user = new FrontendUserAuthentication();
+        $GLOBALS['TSFE']->fe_user->id = 1;
+        $this->expectException(StopActionException::class);
+        $controller->processRequest($request, $response);
     }
 
     /**
@@ -42,6 +69,22 @@ class BasketControllerTest extends FunctionalTestCase
      */
     public function canMainAction()
     {
-        // TODO implement
+        $templateHtml = '<html>
+            count:{countDocs}
+            entries:<f:count subject="{entries}"/>
+        </html>';
+        $controller = $this->setUpController(BasketController::class, [], $templateHtml);
+        $request = $this->setUpRequest('main', []);
+        $response = $this->getResponse();
+        $GLOBALS['TSFE']->fe_user = new FrontendUserAuthentication();
+        $GLOBALS['TSFE']->fe_user->id = 1;
+
+        $controller->processRequest($request, $response);
+        $actual =  $response->getContent();
+        $expected = '<html>
+            count:0
+            entries:0
+        </html>';
+        $this->assertEquals($expected, $actual);
     }
 }
