fadad96eb9575921b53872e2a49f05477992c7ee
Oliver Stöhr <stoehr@effective-webwork.de>
2023-03-30 21:30:35 +0200

    Improve ListViewControllerTest

--- Tests/Functional/Controller/ListViewControllerTest.php
+++ Tests/Functional/Controller/ListViewControllerTest.php
@@ -12,19 +12,24 @@
 namespace Kitodo\Dlf\Tests\Functional\Controller;
 
 use Kitodo\Dlf\Controller\ListViewController;
-use Kitodo\Dlf\Tests\Functional\FunctionalTestCase;
-use TYPO3\CMS\Extbase\Mvc\Response;
-use TYPO3\CMS\Extbase\Mvc\Request;
-use TYPO3\CMS\Extbase\Mvc\View\GenericViewResolver;
-use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 
-class ListViewControllerTest extends FunctionalTestCase
+class ListViewControllerTest extends AbstractControllerTest
 {
+    static array $databaseFixtures = [
+        __DIR__ . '/../../Fixtures/Controller/pages.xml',
+        __DIR__ . '/../../Fixtures/Controller/solrcores.xml'
+    ];
+
+    static array $solrFixtures = [
+        __DIR__ . '/../../Fixtures/Controller/documents.solr.json'
+    ];
+
     public function setUp(): void
     {
         parent::setUp();
-
+        $this->setUpData(self::$databaseFixtures);
+        $this->setUpSolr(4, 2, self::$solrFixtures);
     }
 
     /**
@@ -33,48 +38,40 @@ class ListViewControllerTest extends FunctionalTestCase
      */
     public function canMainAction(): void
     {
-        $request = new Request();
-        $request->setPluginName('ListView');
-        $request->setControllerExtensionName('ListViewControllerTest');
-        $request->setControllerName('ListView');
-        $request->setFormat('html');
-
-        $request->setControllerActionName('main');
-        $request->setArgument('searchParameter', 'test');
-
-        /** @var  ListViewController $subject */
-        $subject = $this->get(ListViewController::class);
-        $response =  $this->objectManager->get(Response::class);
-
-        $GLOBALS['TSFE']->fe_user = new FrontendUserAuthentication();
-
-        $view = new StandaloneView();
-        $view->setTemplateSource(
-            '<html xmlns:v="http://typo3.org/ns/FluidTYPO3/Vhs/ViewHelpers">
+        $arguments = [
+            'searchParameter' => [
+                'query' => '10 Keyboard pieces',
+            ]
+        ];
+        $settings = [
+            'solrcore' => 4,
+            'dont_show_single' => 'some_value',
+            'randomize' => ''
+        ];
+        $templateHtml = '<html xmlns:v="http://typo3.org/ns/FluidTYPO3/Vhs/ViewHelpers">
                 <f:spaceless>
                 uniqueId-length: <v:count.bytes>{viewData.uniqueId}</v:count.bytes>
                 page: {viewData.requestData.page}
                 double: {viewData.requestData.double}
                 widgetPage: {widgetPage.currentPage}
-                lastSearch.query: {lastSearch}
+                lastSearch.query: {lastSearch.query}
                 numResults: {numResults}
                 </f:spaceless>
-            </html>'
-        );
-        $viewResolverMock = $this->getMockBuilder( GenericViewResolver::class)->disableOriginalConstructor()->getMock();
-        $viewResolverMock->expects(self::once())->method('resolve')->willReturn($view);
-        $subject->injectViewResolver($viewResolverMock);
+            </html>';
+        $request = $this->setUpRequest('main', $arguments);
+        $controller = $this->setUpController(ListViewController::class, $settings, $templateHtml);
+        $response =  $this->getResponse();
+        $GLOBALS['TSFE']->fe_user = new FrontendUserAuthentication();
 
-        // Test run
-        $subject->processRequest($request, $response);
+        $controller->processRequest($request, $response);
         $actual =  $response->getContent();
         $expected = '<html xmlns:v="http://typo3.org/ns/FluidTYPO3/Vhs/ViewHelpers">
                 uniqueId-length: 13
                 page: 1
                 double: 0
                 widgetPage: 1
-                lastSearch.query: test
-                numResults: 0
+                lastSearch.query: 10 Keyboard pieces
+                numResults: 1
             </html>';
         $this->assertEquals($expected, $actual);
     }
