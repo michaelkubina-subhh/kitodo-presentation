a98c36eb99688a9b896a929e01055427a16afb21
Oliver Stöhr <stoehr@effective-webwork.de>
2023-04-20 09:52:40 +0200

    Add tests for StatisticsController

--- Tests/Functional/Controller/StatisticsControllerTest.php
+++ Tests/Functional/Controller/StatisticsControllerTest.php
@@ -11,13 +11,21 @@
 
 namespace Kitodo\Dlf\Tests\Functional\Controller;
 
-use Kitodo\Dlf\Tests\Functional\FunctionalTestCase;
+use Kitodo\Dlf\Controller\StatisticsController;
+use TYPO3\CMS\Core\Localization\LanguageService;
 
-class StatisticsControllerTest extends FunctionalTestCase {
+class StatisticsControllerTest extends AbstractControllerTest {
+
+    static array $databaseFixtures = [
+        __DIR__ . '/../../Fixtures/Controller/pages.xml',
+        __DIR__ . '/../../Fixtures/Controller/solrcores.xml',
+        __DIR__ . '/../../Fixtures/Controller/documents.xml'
+    ];
 
     public function setUp(): void
     {
         parent::setUp();
+        $this->setUpData(self::$databaseFixtures);
     }
 
     /**
@@ -25,6 +33,23 @@ class StatisticsControllerTest extends FunctionalTestCase {
      */
     public function canMainAction()
     {
-        // TODO implement
+        $GLOBALS['LANG'] = LanguageService::create('default');
+
+        $settings = [
+            'solrcore' => 4,
+            'collections' => '1',
+            'storagePid' => '0',
+            'description' => 'There are ###TITLES### and ###VOLUMES###.'
+        ];
+        $templateHtml = '<html>{content}</html>';
+
+        $request = $this->setUpRequest('main');
+        $response = $this->getResponse();
+        $controller = $this->setUpController(StatisticsController::class, $settings, $templateHtml);
+
+        $controller->processRequest($request, $response);
+        $actual = $response->getContent();
+        $expected = '<html>There are 3 titles and 3 volumes.</html>';
+        $this->assertEquals($expected, $actual);
     }
 }
