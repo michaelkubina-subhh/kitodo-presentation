0be71e443a0e4a2ef59dbb440235858329fbb78a
Michael Kubina <michael.kubina@sub.uni-hamburg.de>
2025-12-05 13:11:47 +0100

    full recursion in toc

--- Classes/Controller/TableOfContentsController.php
+++ Classes/Controller/TableOfContentsController.php
@@ -147,7 +147,8 @@ class TableOfContentsController extends AbstractController
             // 2. Current menu node points to another file
             // 3. Current menu node has no corresponding images
             if (
-                $entryArray['ITEM_STATE'] == 'CUR'
+                true
+                || $entryArray['ITEM_STATE'] == 'CUR'
                 || (array_key_exists('points', $entry) && is_string($entry['points']))
                 || empty($this->document->getCurrentDocument()->smLinks['l2p'][$entry['id']])
             ) {
